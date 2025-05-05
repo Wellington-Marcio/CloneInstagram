55c02752bc755cadea58273f979fdfff022529c4
Wellington Martins <96148724+Wellington-Marcio@users.noreply.github.com>
2025-05-05 16:08:05 +0100

    updating branch

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+
+    const seguir="Siga a Pagina https://www.instagram.com/casarefugiododouro/"
+    const enviar_msg = "Faça a sua ja a sua reserva, Wts: +351 932471505 !"
+    const add = "Compartilhe nossa pagina, https://www.instagram.com/casarefugiododouro/ ."
+    const setup = "Entre em contacto para mais informações, Wts: +351 932471505 !"
+    
